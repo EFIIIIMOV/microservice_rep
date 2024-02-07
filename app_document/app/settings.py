69a57f9a9ec85b799c2e83e87f6735d1018bf4b9
Andrey <kflyj90@mail.ru>
2024-02-07 14:12:23 +0300

    pr 7 version 1.14 (CI/CD DH (work) YC)

--- app_document/app/settings.py
+++ app_document/app/settings.py
@@ -1,12 +1,37 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
+# from pydantic_settings import BaseSettings, SettingsConfigDict
+#
+#
+# class Settings(BaseSettings):
+#     amqp_url: str
+#     postgres_url_ord: str
+#     postgres_url_doc: str
+#
+#     model_config = SettingsConfigDict(env_file='.env')
+#
+#
+# settings = Settings()
+
+
+from pathlib import Path
+import os
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
+
+# Определяем путь к .env файлу относительно расположения settings.py
+env_path = Path(__file__).resolve().parents[1] / ".env"
+load_dotenv(dotenv_path=env_path)
+
+print(f"Current working directory: {os.getcwd()}")
+print(f"Is .env file present: {os.path.isfile(env_path)}")
+print(f"env_path: {env_path}")
 
 
 class Settings(BaseSettings):
-    amqp_url: str
-    postgres_url_ord: str
-    postgres_url_doc: str
+    amqp_url: str = os.getenv("AMQP_URL")
+    print(f"\n\n AMQP_URL: {amqp_url}\n\n")
 
-    model_config = SettingsConfigDict(env_file='.env')
+    postgres_url: str = os.getenv("POSTGRES_URL_DOC")
+    print(f"\n\n POSTGRES_URL_DOC: {postgres_url}\n\n")
 
 
 settings = Settings()

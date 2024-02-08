bc398eb92a230c9ca817cc745e4596ae4e52bf1b
Andrey <kflyj90@mail.ru>
2024-02-08 21:48:45 +0300

    pr 8 version 0.4.3 + ci-cd DH (work) YC + app_order tests unit + integration (work) + app_document tests unit + integration (work)

--- app_order/migration/env.py
+++ app_order/migration/env.py
@@ -4,7 +4,6 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from sqlalchemy_utils import database_exists, create_database
 
-
 from alembic import context
 
 # this is the Alembic Config object, which provides
@@ -18,8 +17,8 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-from app.schemas.order import Order
-from app.schemas.base_schema import Base
+from app_order.app.schemas.order import Order
+from app_order.app.schemas.base_schema import Base
 
 target_metadata = Base.metadata
 
@@ -29,7 +28,7 @@ target_metadata = Base.metadata
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-from app.settings import settings
+from app_order.app.settings import settings
 
 
 def run_migrations_offline() -> None:

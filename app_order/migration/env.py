4b0496761906efd320e4272f3cd284fc5ad3d792
Andrey <kflyj90@mail.ru>
2024-02-10 13:26:46 +0300

    pr 9 version 0.0.1 + keycloak

--- app_order/migration/env.py
+++ app_order/migration/env.py
@@ -16,7 +16,7 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-from app_order.app.schemas.base_schema import Base
+from app.schemas.base_schema import Base
 
 target_metadata = Base.metadata
 
@@ -26,7 +26,7 @@ target_metadata = Base.metadata
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-from app_order.app.settings import settings
+from app.settings import settings
 
 
 def run_migrations_offline() -> None:
